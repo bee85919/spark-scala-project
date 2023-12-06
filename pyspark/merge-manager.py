8a296cbd401e7e663a26a0044aee4567d6215161
bee85919 <bee85919@gmail.com>
2023-12-07 01:27:30 +0900

    Create merge-manager.py

---
+++ pyspark/merge-manager.py
@@ -0,0 +1,18 @@
+###
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+###
+spark = SparkSession.builder.appName("CSV Merge").getOrCreate()
+
+###
+root_path = "/Users/b06/Desktop/yeardream/medi-05/spark-scala-project/output"
+csvs_path = f"{root_path}/pyspark/naverplace"
+save_path = f"{root_path}/result"
+
+###
+df = spark.read.option("header", "true").option("encoding", "UTF-8").csv(csvs_path + "/*.csv")
+df.coalesce(1).write.option("header", "true").option("encoding", "UTF-8").csv(save_path)
+
+###
+spark.stop()
